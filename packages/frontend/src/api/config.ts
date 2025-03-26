0724c57819e21e29ee6911e1b7c1a03f181f41a6
ahmed <ahmedehab.sg@gmail.com>
2025-03-26 14:56:53 +0200

    adj cors

--- packages/frontend/src/api/config.ts
+++ packages/frontend/src/api/config.ts
@@ -1,9 +1,10 @@
 const apiConfig = {
   baseUrl: import.meta.env.VITE_API_BASE_URL,
   headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "*",
-    "Access-Control-Allow-Headers": "",
+    "Access-Control-Allow-Origin": import.meta.env.VITE_API_BASE_URL,
+    "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE",
+    "Access-Control-Allow-Headers":
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
   },
 };
 
