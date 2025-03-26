93e72b61a2360ff446281a12b09c8177be43001f
ahmed <ahmedehab.sg@gmail.com>
2025-03-26 15:46:23 +0200

    test config

--- packages/frontend/src/api/config.ts
+++ packages/frontend/src/api/config.ts
@@ -1,7 +1,7 @@
 const apiConfig = {
-  baseUrl: import.meta.env.VITE_API_BASE_URL,
+  baseUrl: "http://localhost:8080",
   headers: {
-    "Access-Control-Allow-Origin": import.meta.env.VITE_API_BASE_URL,
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE",
     "Access-Control-Allow-Headers": "authorization",
   },
